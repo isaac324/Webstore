4ab5aa0fd5bd824803436444d33973cd390ce156
Isaac <isaavila@csumb.edu>
2019-09-13 10:55:12 -0700

    add sigin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { SigninComponent } from './signin/signin.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ListComponent,
-    SigninComponent
+    SigninComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
