1157fd8b20d6d59b3773333b9d14be149bdef4db
Isaac <isaavila@csumb.edu>
2019-09-24 19:30:49 -0700

    add files 9/24

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,20 @@ import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 import { SigninComponent } from './signin/signin.component';
 
+import { DetailComponent } from './detail/detail.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { FinalizeComponent } from './finalize/finalize.component';
+
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'list', component: ListComponent },
-  {path: 'signin', component: SigninComponent }
+  {path: 'signin', component: SigninComponent },
+  {path: 'detail', component: DetailComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'checkout', component: CheckoutComponent},
+  {path: 'finalize', component: FinalizeComponent}
 ];
 
 @NgModule({
